62c4f91d62c77eca7013edf36065f8b9320786c7
Ctibor Laky <luckylooke@gmail.com>
2017-08-05 10:16:28 +0200

    vanilla router

--- dragon-web/src/index.js
+++ dragon-web/src/index.js
@@ -1,125 +1,28 @@
-import './app.css';
-import './../node_modules/flexboxgrid/dist/flexboxgrid.css';
-import './../../dist/dragon.css';
+import './app.css'
+import { homeHandler } from '../pages/home.js'
+import { createRouter } from 'vanilla-ui-router'
 
-import dragon from './../../src/dragon.dev.lib'
+// Initialize the router with the dynamic DOM entry point
+const router = createRouter( document.getElementById('app') );
 
-buildExample( GET_MOCK_INPUT_GENERATED() )
+// https://github.com/micromata/vanilla-ui-router
+router
 
-// definitions
-
-function buildExample( containers ){
-
-	var exampleElm = document.getElementById('example')
-	createContainers( containers, exampleElm )
-
-	let d = dragon(document.getElementsByClassName('container'))
-      console.log('dingdong', d)
-}
-
-function createContainers( containers, targetElement, level ) {	
-
-	level = level || 0
-
-	var rowElm = document.createElement('div')
-  	rowElm.className = 'row'
-
-	containers.forEach( function( container ){
-
-		var colElm = document.createElement('div')
-		colElm.className = level > 0 ? 'col-xs-10 container' : 'col-xs-12 col-sm-6 col-md-4 col-lg-3 container'
-		// colElm.innerHTML = '<p>' + container.value + '</p>'
-
-		if ( container.items ){
-
-			container.items.forEach( function( item ){
-
-				var boxElm = document.createElement('div')
-				boxElm.className = 'box'
-				boxElm.innerHTML = '<p>' + item.value + '</p>'
-
-				if ( item.items ) // nested containers
-					createContainers( [item], boxElm, level + 1 )
-
-				colElm.appendChild( boxElm )
-			})
-		}
-
-	  	rowElm.appendChild( colElm )
+	// Start route: The server side URL without a hash
+	.addRoute('', () => {
+		/*
+			Use navigateTo(…) to make dynamic route changes, i.e. to redirect to another route
+		*/
+		router.navigateTo('home');
 	})
 
-	targetElement.appendChild( rowElm )
+	.addRoute('home', {
+		templateUrl: './pages/home.html',
+		routeHandler: homeHandler
+	})
 
-}
-
-function GET_MOCK_INPUT(){
-
-	return [
-
-		{
-			items: [{
-				value: 'item 1'
-			}, {
-				value: 'item 2'
-			}, {
-				value: 'item 3',
-
-				items: [{
-					value: 'item 31'
-				}, {
-					value: 'item 32'
-				}, {
-					value: 'item 33'
-				}, {
-					value: 'item 34'
-				}]
-			}, {
-				value: 'item 4'
-			}]
-		},
-
-		{
-			items: [{
-				value: 'item 5'
-			}, {
-				value: 'item 6'
-			}, {
-				value: 'item 7'
-			}, {
-				value: 'item 8'
-			}]
-		}
-	]
-}
-
-function GET_MOCK_INPUT_GENERATED(){
-
-	return [
-		getRandomContainerData(),
-		getRandomContainerData(),
-		getRandomContainerData(),
-		getRandomContainerData(),
-	]
-}
-
-function getRandomContainerData( data, level ){
-
-	var num = 1 + Math.round( Math.random() *2 +3 )
-
-	data = data || {}
-	level = level || 0
-
-	data.items = []
-
-	for ( var i = 0; i < num; i++) {
-
-		var item = {value: 'item #' + Math.round( Math.random() *10000 )}
-
-		if ( level < 3 && Math.random() < 0.1 ) // make nested
-			item = getRandomContainerData( item, level + 1 )
-
-		data.items.push( item )
-	}
-
-	return data
-}
+	.otherwise(() => {
+		// If no route configuration matches, the otherwise route is invoked.
+		console.log('I am the otherwise route');
+		router.navigateTo('404');
+	});
